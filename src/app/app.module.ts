346933cf531b6b2fc844970944a534785cefe011
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-09-20 12:36:19 +0200

    Add very simple Bluetooth demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { WebBluetoothModule } from '@manekinekko/angular-web-bluetooth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebBluetoothModule.forRoot({
+      enableTracing: true // or false, this will enable logs in the browser's console
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+import { Component, NgZone, OnInit } from '@angular/core';
+
+import { BatteryLevelService } from '../_services/battery-level.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  batteryLevel: string = '--';
+  device: any = {};
+
+  constructor(
+    public _zone: NgZone,
+    public _batteryLevelService: BatteryLevelService
+  ) { }
+
+  ngOnInit() {
+    this.getDeviceStatus();
+    this.streamValues();
+  }
+
+  streamValues() {
+    this._batteryLevelService.stream().subscribe(this.showBatteryLevel.bind(this));
+  }
+
+  getDeviceStatus() {
+    this._batteryLevelService.getDevice().subscribe(
+      (device) => {
+
+        if (device) {
+          this.device = device;
+        }
+        else {
+          // device not connected or disconnected
+          this.device = null;
+          this.batteryLevel = '--';
+        }
+      }
+    );
+  }
+
+  getFakeValue() {
+    this._batteryLevelService.getFakeValue();
+  }
+
+  getBatteryLevel() {
+    return this._batteryLevelService.value().subscribe(this.showBatteryLevel.bind(this));
+  }
+
+  showBatteryLevel(value: number) {
+
+    // force change detection
+    this._zone.run(() => {
+      console.log('Reading battery level %d', value);
+      this.batteryLevel = '' + value;
+    });
+  }
+}
