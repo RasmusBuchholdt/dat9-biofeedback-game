f7d7d6138a61761dfb13146c61b4de2d9d8af025
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-10-17 19:13:24 +0200

    Ovewrite calibration in flying game

--- src/app/games/flying-game/flying-game.component.ts
+++ src/app/games/flying-game/flying-game.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import {
+  PushHardCalibration,
+} from 'src/app/_models/calibration/push-hard-calibration';
+import {
   FlyingGameEngineService,
 } from 'src/app/_services/flying-game-engine.service';
 import { SpiromagicService } from 'src/app/_services/spiromagic.service';
@@ -35,6 +38,7 @@ export class FlyingGameComponent implements OnInit {
   }
 
   getReadings(): void {
+    this.spiromagicService.calibration$.next(new PushHardCalibration())
     this.subscription = this.spiromagicService.reading$.subscribe(reading => {
       this.gameEngine.updatePlane(reading);
     })
