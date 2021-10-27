03dd0a36b0e6dc7e697d02f0609e29253849be9e
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-10-27 11:22:12 +0200

    Base game of flying game

--- src/app/games/kiwi-game/kiwi-game.component.ts
+++ src/app/games/kiwi-game/kiwi-game.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import {
+  PushHardCalibration,
+} from 'src/app/_models/calibration/push-hard-calibration';
+import {
+  KiwiGameEngineService,
+} from 'src/app/_services/kiwi-game-engine.service';
+import { SpiromagicService } from 'src/app/_services/spiromagic.service';
 
 @Component({
   selector: 'app-kiwi-game',
@@ -7,9 +15,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KiwiGameComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('rendererCanvas', { static: true })
+  public rendererCanvas: ElementRef<HTMLCanvasElement> | undefined;
+  public screenshotBase64: ArrayBuffer | string;
+
+  private subscription: Subscription | null;
+
+  constructor(
+    private gameEngine: KiwiGameEngineService,
+    private spiromagicService: SpiromagicService
+  ) { }
 
   ngOnInit(): void {
+    this.gameEngine.createScene(this.rendererCanvas);
+    this.gameEngine.animate();
+    this.getReadings();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    this.gameEngine.stopGame();
+  }
+
+  getReadings(): void {
+    this.spiromagicService.calibration$.next(new PushHardCalibration())
+    this.subscription = this.spiromagicService.reading$.subscribe(reading => {
+      // this.gameEngine.updatePlane(reading);
+      this.gameEngine.updatePlaneSmooth(reading);
+    })
+  }
 }
