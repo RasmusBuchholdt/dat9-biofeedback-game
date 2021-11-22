db280ce4c40125ba5aefce3c57c146412877807d
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-11-22 21:33:07 +0100

    Add support for cached baseline (still no way to clear it)

--- src/app/_models/calibration/constant-stepper-calibration.ts
+++ src/app/_models/calibration/constant-stepper-calibration.ts
@@ -4,56 +4,68 @@ import { CalibrationBase } from './calibration-base';
 import { CalibrationStrategy } from './calibration-strategy';
 
 export class ConstantStepperCalibration extends CalibrationBase implements CalibrationStrategy {
-    
-    // Baseline related variables
-    private baseline = 0;
-    private minBaselineCounter = 20;
-    private numberOfIterations = 0;
 
-    // Calibration related variables
-    private previousReading = 0;
-    private stepValue = 0;
-    private stepIncrement = 1;
-    private maxValue = 100;
-    private minValue = 0;
-    
-    get name(): string {
-        return "Constant stepper";
+  // Baseline related variables
+  private baseline = 0;
+  private minBaselineCounter = 20;
+  private numberOfIterations = 0;
+
+  // Calibration related variables
+  private previousReading = 0;
+  private stepValue = 0;
+  private stepIncrement = 3;
+  private maxValue = 100;
+  private minValue = 0;
+
+  private BASELINE_KEY = 'CONSTANT_STEPPER_BASELINE';
+
+  get name(): string {
+    return "Constant stepper";
+  }
+
+  get description(): string {
+    return "Stepper which stays at last known location.";
+  }
+
+  calibrate(reading: number, minReading: number, maxReading: number, sensitivity: number): number {
+
+    // Initialization
+    const currentReading = Math.round(normalize(reading, minReading, maxReading));
+
+    // Find baseline
+    this.findBaseline(currentReading);
+
+    // Find step value
+    if (currentReading > this.baseline + 1 && this.stepValue < this.maxValue)
+      this.stepValue += this.stepIncrement;
+    else if (currentReading < this.baseline - 1 && this.stepValue > this.minValue)
+      this.stepValue -= this.stepIncrement;
+
+    // Output
+    this.previousReading = currentReading;
+    return this.stepValue;
+  }
+
+  // Find baseline (value when no exhale or inhale has been made)
+  // A value is first seen as a baseline after "minBaselineCounter" number of iterations.
+  private findBaseline(reading: number): void {
+    const content = localStorage.getItem(this.BASELINE_KEY);
+    if (content !== null) {
+      this.baseline = JSON.parse(content) as number;
+      return;
     }
 
-    get description(): string {
-        return "Stepper which stays at last known location.";
+    if (reading == this.previousReading) {
+      this.numberOfIterations += 1;
+      if (this.numberOfIterations > this.minBaselineCounter)
+        this.setBaseline(reading);
+    } else {
+      this.numberOfIterations = 0;
     }
-    
-    calibrate(reading: number, minReading: number, maxReading: number, sensitivity: number): number {
-        
-        // Initialization
-        const currentReading = Math.round(normalize(reading, minReading, maxReading))
-        
-        // Find baseline
-        this.findBaseline(currentReading)
-        
-        // Find step value
-        if(currentReading > this.baseline + 1 && this.stepValue < this.maxValue)
-            this.stepValue += this.stepIncrement;
-        else if(currentReading < this.baseline - 1 && this.stepValue > this.minValue)
-            this.stepValue -= this.stepIncrement;
+  }
 
-        // Output
-        this.previousReading = currentReading;
-        return this.stepValue;
-    }
-
-    // Find baseline (value when no exhale or inhale has been made)
-    // A value is first seen as a baseline after "minBaselineCounter" number of iterations.
-    findBaseline(reading: number): void {
-        
-        if(reading == this.previousReading)
-        {
-            this.numberOfIterations += 1;
-            if(this.numberOfIterations > this.minBaselineCounter) this.baseline = reading;
-        }
-        else this.numberOfIterations = 0;
-    }
-    
+  private setBaseline(reading: number): void {
+    localStorage.setItem(this.BASELINE_KEY, JSON.stringify(reading));
+    this.baseline = reading;
+  }
 }
