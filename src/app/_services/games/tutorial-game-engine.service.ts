68e2d89aa9e59681813ad012a9aeb66ef10ab944
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-11-23 12:52:54 +0100

    Add tutorial game concept

--- src/app/_services/games/tutorial-game-engine.service.ts
+++ src/app/_services/games/tutorial-game-engine.service.ts
@@ -62,7 +62,8 @@ export class TutorialGameEngineService {
     this.camera = new THREE.PerspectiveCamera(
       75, window.innerWidth / window.innerHeight, 0.1, 1000
     );
-    this.camera.position.z = 5;
+    this.camera.position.z = 7;
+    this.camera.position.y = 1;
     this.scene.add(this.camera);
 
     const skyColor = 0xB1E1FF;
--- src/app/games/tutorial-game/tutorial-game.component.ts
+++ src/app/games/tutorial-game/tutorial-game.component.ts
@@ -5,7 +5,9 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
+import { CalibrationReadings } from 'src/app/_models/calibration-readings';
 import {
   TutorialGameEngineService,
 } from 'src/app/_services/games/tutorial-game-engine.service';
@@ -24,15 +26,29 @@ export class TutorialGameComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription | null;
 
+  private readingTarget: number;
+  private exhalationCompleted = false;
+  private inhalationCompleted = false;
+
+  private toastrConfig = {
+    timeOut: 10000,
+    positionClass: 'toast-bottom-center',
+    enableHtml: true
+  };
+
+  readings: CalibrationReadings | null = null;
+
   constructor(
     private gameEngine: TutorialGameEngineService,
-    private spiromagicService: SpiromagicService
+    private spiromagicService: SpiromagicService,
+    private toastrService: ToastrService
   ) { }
 
   ngOnInit(): void {
     this.gameEngine.createScene(this.rendererCanvas);
     this.gameEngine.animate();
     this.getReadings();
+    this.start();
   }
 
   ngOnDestroy(): void {
@@ -42,7 +58,26 @@ export class TutorialGameComponent implements OnInit, OnDestroy {
 
   getReadings(): void {
     this.subscription = this.spiromagicService.reading$.subscribe(reading => {
-      this.gameEngine.setInnerCircle(reading);
+      this.handleReading(reading);
     });
+  }
+
+  private start(): void {
+    this.readingTarget = 100;
+    this.toastrService.success(`Exhale until the circle is full and you hear a pling.`, 'Exhalation', this.toastrConfig);
+  }
+
+  private handleReading(value: number): void {
+    this.gameEngine.setInnerCircle(value);
+
+    if (value === this.readingTarget && !this.exhalationCompleted) {
+      this.exhalationCompleted = true;
+      this.readingTarget = 0;
+      this.toastrService.success(`Inhale until the circle is empty and you hear a pling.`, 'Inhalation', this.toastrConfig);
+    } else if (value === this.readingTarget && !this.inhalationCompleted) {
+      this.inhalationCompleted = true;
+      this.toastrService.success(`You have successfully completed the tutorial.`, 'Completed', this.toastrConfig);
+      this.spiromagicService.markTutorialAsCompleted();
+    }
   }
 }
