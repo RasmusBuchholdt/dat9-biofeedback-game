2127b98e29eb543681420ad6962c78e251231a67
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-11-30 12:16:22 +0100

    Add logic to tutorial component

---
+++ src/app/_services/tutorial-engine.service.ts
@@ -0,0 +1,163 @@
+import { ElementRef, Injectable, NgZone } from '@angular/core';
+import * as THREE from 'three';
+
+import { scaleNumberToRange } from '../_utils/scale-number-to-range';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TutorialEngineService {
+  private canvas: HTMLCanvasElement;
+  private renderer: THREE.WebGLRenderer;
+  private camera: THREE.PerspectiveCamera;
+  private scene: THREE.Scene;
+  private light: THREE.HemisphereLight;
+
+  private circleMaxValue = 3;
+  private circleMinValue = 0.1;
+  private biggestValue = 0;
+
+  private innerCircle: THREE.Mesh;
+  private outerCircle: THREE.Mesh;
+  private maxValueCircle: THREE.Mesh;
+
+  private frameId: number = null;
+
+  constructor(
+    private ngZone: NgZone
+  ) { }
+
+  stopGame(): void {
+    if (this.frameId != null) {
+      cancelAnimationFrame(this.frameId);
+    }
+    if (this.scene != null) {
+      this.renderer.dispose();
+      this.scene.children.forEach(this.killObject); // Remove children, but also their materials.
+      this.scene.clear(); // This does remove all the children too, but does not dispose the materials (I think).
+      this.scene = null;
+    }
+  }
+
+  createScene(canvas: ElementRef<HTMLCanvasElement>): void {
+    this.canvas = canvas.nativeElement;
+
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true,
+      antialias: true
+    });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color('#323f4f');
+
+    this.camera = new THREE.PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 0.1, 1000
+    );
+    this.camera.position.z = 10;
+    this.camera.position.y = 1;
+    this.scene.add(this.camera);
+
+    const skyColor = 0xB1E1FF;
+    const groundColor = 0xB97A20;
+    const intensity = 1.5;
+    this.light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+    this.scene.add(this.light);
+
+    this.addCircles();
+  }
+
+  private addCircles(): void {
+    let geometry = new THREE.CircleGeometry(this.circleMaxValue, 32);
+    let material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    this.outerCircle = new THREE.Mesh(geometry, material);
+    this.scene.add(this.outerCircle);
+
+    geometry = new THREE.CircleGeometry(this.circleMinValue, 32);
+    material = new THREE.MeshBasicMaterial({ color: 'green' });
+    this.innerCircle = new THREE.Mesh(geometry, material);
+    this.scene.add(this.innerCircle);
+  }
+
+  setInnerCircle(value: number) {
+    const scaledValue = scaleNumberToRange(value, 0, 100, this.circleMinValue, this.circleMaxValue);
+    let geometry = new THREE.CircleGeometry(scaledValue, 32);
+    let material = new THREE.MeshBasicMaterial({ color: 'green' });
+    this.scene.remove(this.innerCircle);
+    this.innerCircle = new THREE.Mesh(geometry, material);
+    this.scene.add(this.innerCircle);
+
+    if (scaledValue > this.biggestValue) this.adjustMaxValueCircle(scaledValue);
+  }
+
+  private adjustMaxValueCircle(value: number): void {
+    this.biggestValue = value;
+    let geometry = new THREE.CircleGeometry(value, 32);
+    let material = new THREE.MeshBasicMaterial({ color: 'red' });
+    this.scene.remove(this.maxValueCircle);
+    this.maxValueCircle = new THREE.Mesh(geometry, material);
+    this.scene.add(this.maxValueCircle);
+  }
+
+  animate(): void {
+    this.ngZone.runOutsideAngular(() => {
+      if (document.readyState !== 'loading') {
+        this.render();
+      } else {
+        window.addEventListener('DOMContentLoaded', () => {
+          this.render();
+        });
+      }
+      window.addEventListener('resize', () => {
+        this.resize();
+      });
+    });
+  }
+
+  private render(): void {
+    this.frameId = requestAnimationFrame(() => {
+      this.render();
+    });
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  private killObject(object: (THREE.Object3D | THREE.HemisphereLight | THREE.Mesh) & { isMesh: boolean, material: any, geometry: THREE.BoxGeometry }): void {
+    object.clear();
+    if (object.isMesh) {
+      object.geometry.dispose()
+      if (object.material.type == 'MeshBasicMaterial') {
+        return;
+      }
+      if (object.material.isMaterial) {
+        this.cleanMaterial(object.material)
+      } else {
+        for (const material of object.material) this.cleanMaterial(material)
+      }
+    }
+  }
+
+  private cleanMaterial(material: any): void {
+    material.dispose()
+    // dispose textures
+    for (const key of Object.keys(material)) {
+      const value = material[key]
+      if (value && typeof value === 'object' && 'minFilter' in value) {
+        value.dispose()
+      }
+    }
+  }
+
+  private resize(): void {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height);
+  }
+
+  takeSceneScreenshot(): string {
+    this.renderer.render(this.scene, this.camera);
+    return this.renderer.domElement.toDataURL();
+  }
+}
--- src/app/pages/tutorial/tutorial.component.ts
+++ src/app/pages/tutorial/tutorial.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { SpiromagicService } from 'src/app/_services/spiromagic.service';
+import {
+  TutorialEngineService,
+} from 'src/app/_services/tutorial-engine.service';
 
 @Component({
   selector: 'app-tutorial',
@@ -8,13 +13,43 @@ import { SpiromagicService } from 'src/app/_services/spiromagic.service';
 })
 export class TutorialComponent implements OnInit {
 
+  @ViewChild('rendererCanvas', { static: true })
+  public rendererCanvas: ElementRef<HTMLCanvasElement> | undefined;
+
   tutorialStarted = false;
+  sensitivity: number;
+
+  private subscriptions: Subscription[] = [];
 
   constructor(
-    public spiromagicService: SpiromagicService
+    public spiromagicService: SpiromagicService,
+    private tutorialEngine: TutorialEngineService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.setupSpirometer();
   }
 
+  private setupSpirometer(): void {
+    this.subscriptions.push(this.spiromagicService.sensitivity$.subscribe(sensitivity => {
+      this.sensitivity = sensitivity;
+    }));
+  }
+
+  start(): void {
+    this.tutorialStarted = true;
+    this.tutorialEngine.createScene(this.rendererCanvas);
+    this.tutorialEngine.animate();
+  }
+
+  finish(): void {
+    this.spiromagicService.setTutorialCompleted(true);
+    this.tutorialEngine.stopGame();
+    this.router.navigateByUrl('menu');
+  }
+
+  changeSensitivity(value: number): void {
+    this.spiromagicService.sensitivity$.next(value);
+  }
 }
