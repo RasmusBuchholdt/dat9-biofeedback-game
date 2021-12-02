4c3045d57996226f7dadc6f882b5eecab312a098
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-12-02 12:36:11 +0100

    Add extra calibration step with progression bar

--- src/app/pages/calibration/calibration.component.ts
+++ src/app/pages/calibration/calibration.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { SpiromagicService } from 'src/app/_services/spiromagic.service';
 
 @Component({
@@ -7,20 +8,30 @@ import { SpiromagicService } from 'src/app/_services/spiromagic.service';
   templateUrl: './calibration.component.html',
   styleUrls: ['./calibration.component.scss']
 })
-export class CalibrationComponent implements OnInit {
+export class CalibrationComponent implements OnInit, OnDestroy {
 
+  calibrationProgress = 0;
   exhalationCompleted = false;
   inhalationCompleted = false;
 
+  private subscription: Subscription | null = null;
+
   constructor(
-    public spiromagicService: SpiromagicService,
+    private spiromagicService: SpiromagicService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    // TODO: Maybe remove this or move to better place?
     if (this.spiromagicService.tutorialFinished$.getValue()) {
       this.router.navigateByUrl('/menu');
-    };
+    } else {
+      this.subscription = this.spiromagicService.calibrationProgress$.subscribe(calibrationProgress => {
+        this.calibrationProgress = calibrationProgress;
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
