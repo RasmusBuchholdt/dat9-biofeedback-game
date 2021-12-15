b2c0ccbf8a546dde3c140ecdcaff3450da6418b5
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-12-15 15:21:19 +0100

    Sensitivity now increases reading with x%

--- src/app/_models/calibration/dynamic-stepper-calibration.ts
+++ src/app/_models/calibration/dynamic-stepper-calibration.ts
@@ -48,8 +48,7 @@ export class DynamicStepperCalibration extends CalibrationBase implements Calibr
     const stepIncrement = Math.abs(currentReading - this.baseline) * stepIncreasePercentage;
 
     // Find step value
-    // Include the sensitivity here, so it's more likely to go up
-    if (currentReading > this.baseline + 1) {
+    if (currentReading * (sensitivity / 100 + 1) > (this.baseline + 1)) {
       // I have added 0.5 because it seems that exhale requires more force than inhale.
       //this.stepValue += stepIncrement + 0.5;
       this.stepValue += 1.5;
