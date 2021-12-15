1a82513e966c4745a99de0364005f13001569161
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-12-15 11:10:56 +0100

    Add instructions to exercise 1 and 2

--- src/app/applications/balloon/balloon.component.ts
+++ src/app/applications/balloon/balloon.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
 import {
   BalloonEngineService,
@@ -16,16 +15,16 @@ import {
   styleUrls: ['./balloon.component.scss']
 })
 export class BalloonComponent implements OnInit {
-
   @ViewChild('rendererCanvas', { static: true })
+
   public rendererCanvas: ElementRef<HTMLCanvasElement> | undefined;
+  public instructions = "";
 
   private subscription: Subscription | null;
 
   constructor(
     private engine: BalloonEngineService,
-    private spiromagicService: SpiromagicService,
-    private toastrService: ToastrService,
+    private spiromagicService: SpiromagicService
   ) { }
 
   ngOnInit(): void {
@@ -33,30 +32,20 @@ export class BalloonComponent implements OnInit {
     this.engine.animate();
     this.getReadings();
 
-    let hint = '';
-
     if (history.state.guidance) {
       this.engine.guidance = true;
-      hint =
-        `....`;
+      this.instructions = '';
     } else {
-      hint =
+      this.instructions =
         `In this application you will perform the pursed lip breathing exercise like you did in the tutorial.</br>
       When you exhale through the SpiroMagic device, the inner circle will expand.</br>
       Try to expand it enough that it reaches the outer circle.`;
     }
-
-    this.toastrService.info(hint, `Controls`, {
-      disableTimeOut: true,
-      closeButton: true,
-      positionClass: 'toast-bottom-left'
-    });
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
     this.engine.stop();
-    this.toastrService.clear();
   }
 
   getReadings(): void {
